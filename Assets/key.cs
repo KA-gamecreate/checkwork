9df31b517a01e73da5662b1b5d6e4edfbb3cbae1
”KA-gamecreate” <burakki.efi.22@docomo.ne.jp>
2020-07-05 14:08:58 +0900

    ９個終わった

---
+++ Assets/key.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class key : MonoBehaviour
+{
+    public GameObject red;
+    public GameObject blue;
+    public GameObject green;
+    public GameObject door;
+    // Start is called before the first frame update
+    void Start()
+    {
+        red.gameObject.SetActive(true);
+        blue.gameObject.SetActive(false);
+        green.gameObject.SetActive(false);
+        door.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(collision.gameObject);
+        Debug.Log("hit");
+
+        if (collision.gameObject.tag == "red")
+        { 
+            blue.gameObject.SetActive(true);
+        }
+
+        if (collision.gameObject.tag == "blue")
+        {
+            green.gameObject.SetActive(true);
+        }
+
+        if (collision.gameObject.tag == "green")
+        {
+            door.gameObject.SetActive(true);
+        }
+        if (collision.gameObject.tag == "door")
+        {
+            SceneManager.LoadScene("battle");
+        }
+    }
+}
+
+
+
+    
+
---
+++ Assets/player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class player : MonoBehaviour
+{
+
+    public Text text;
+    public int number;
+    public Text damage;
+    public int total;
+    public Text hidame;
+    public int hhidame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        number = 0;
+        hhidame = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+
+    public void CountUp()
+    {
+
+        number = number + 1;
+        text.text = number.ToString();
+        hhidame = hhidame + 1;
+        hidame.text = hhidame.ToString();
+        if (hhidame >= 3)
+        {
+            SceneManager.LoadScene("gameover");
+        }
+
+
+    }
+    public void Attack()
+    {
+        total += number;
+        damage.text = total.ToString();
+
+    }
+}
